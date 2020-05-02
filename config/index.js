3b87340a939a6a9917a09ca3fc60a49d8431298b
Yesh Yendamuri <yeshwanth.yk@gmail.com>
2020-05-02 00:54:01 -0400

    Clean up index.js

--- config/index.js
+++ config/index.js
@@ -2,18 +2,18 @@ module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
 
   siteTitle: 'Yesh', // Navigation and Site Title
-  siteTitleAlt: 'Jodie - Gatsby Starter Portfolio', // Alternative Site title for SEO
-  siteTitleShort: 'Jodie', // short_name for manifest
-  siteHeadline: 'Come & Enjoy our excellent photos', // Headline for schema.org JSONLD
-  siteUrl: 'https://jodie.lekoarts.de', // Domain of your site. No trailing slash!
+  siteTitleAlt: 'Portfolio - Yesh Yendamuri', // Alternative Site title for SEO
+  siteTitleShort: 'Yesh', // short_name for manifest
+  siteHeadline: 'Portfolio of Yesh Yendamuri', // Headline for schema.org JSONLD
+  siteUrl: 'https://yeshyendamuri.com', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
   siteLogo: '/logos/logo.png', // Used for SEO and manifest
-  siteDescription: 'Image-heavy photography portfolio with colorful accents & great typography',
-  author: 'LekoArts', // Author for schema.org JSONLD
+  siteDescription: 'Portfolio of Yesh Yendamuri',
+  author: 'Yesh Yendamuri', // Author for schema.org JSONLD
 
   // siteFBAppID: '123456789', // Facebook App ID - Optional
-  userTwitter: '@jodie', // Twitter Username
-  ogSiteName: 'jodie', // Facebook Site Name
+  userTwitter: '@yesh', // Twitter Username
+  ogSiteName: 'yesh', // Facebook Site Name
   ogLanguage: 'en_US', // og:language
   googleAnalyticsID: 'UA-XXXXXX-X',
 
